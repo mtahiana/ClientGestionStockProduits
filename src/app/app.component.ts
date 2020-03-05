c998cf9cb1fb6c1d8dbf248ee35c34a3fbabe35c
mtahiana <mahatr9@yahoo.fr>
2020-03-05 06:53:45 +0400

    ajout de sidebar et navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  showHideSideBar: boolean = false;
+
+  onShowSideBarChange(showHideSideBar){
+    this.showHideSideBar = showHideSideBar;
+  }
 }
