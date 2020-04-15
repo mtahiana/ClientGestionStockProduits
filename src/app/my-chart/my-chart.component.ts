73d1ab762a03a2d1146d0e0a063fe81a53692116
mtahiana <mahatr9@yahoo.fr>
2020-04-15 21:28:37 +0400

    dashboard_chartjs

---
+++ src/app/my-chart/my-chart.component.ts
@@ -0,0 +1,54 @@
+import { Component, AfterViewChecked, /* OnInit,*/ Input, ViewChild } from '@angular/core';
+
+import { ChartComponent } from 'angular2-chartjs';
+
+@Component({
+  selector: 'app-my-chart',
+  templateUrl: './my-chart.component.html',
+  styleUrls: ['./my-chart.component.css']
+})
+export class MyChartComponent implements OnInit {
+
+  @ViewChild('ghraphElement', {static: true})
+  private ghraphElement: ChartComponent;
+
+  @Input()
+  type: string = 'bar';
+
+  @Input()
+  title: string = 'Titre';
+
+  @Input()
+  data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "My dataset 1",
+        data: [65, 59, 80, 81, 56, 55, 40]
+      },
+      {
+        label: "My dataset 2",
+        data: [5, 59, 80, 55, 56, 55, 25]
+      }
+    ]
+  };
+
+  @Input()
+  options = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+
+  constructor() { }
+
+  /*ngOnInit() {
+    //console.log(this.ghraphElement);
+    this.ghraphElement.chart.update();
+  }*/
+
+  ngAfterViewChecked() {
+      this.ghraphElement.chart.update();
+
+  }
+
+}
